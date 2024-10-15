39c5b33629f43148b6ae9ba7dd4e9dd00b76a109
Andreas Nygard <andreas.nygard@gmail.com>
2024-10-15 23:14:06 +1100

    vim: Move to neovim land

---
+++ .config/nvim/init.lua
@@ -0,0 +1,41 @@
+-------------------
+-- Init
+-------------------
+require("config.lazy")
+require("lazy").setup("plugins")
+
+-------------------
+-- Theme
+-------------------
+vim.g.material_style = 'deep ocean'
+vim.cmd 'colorscheme material'
+
+-------------------
+-- Editor
+-------------------
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+---------------------------------------
+-- Plugins
+---------------------------------------
+require('lualine').setup()
+
+-------------------
+-- Telescope
+-------------------
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-------------------
+-- Oil
+-------------------
+require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,8 @@
+return {
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+    { 'marko-cerovac/material.nvim' },
+    { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-treesitter/nvim-treesitter' },
+    { 'stevearc/oil.nvim', dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { 'tpope/vim-sleuth' },
+}
