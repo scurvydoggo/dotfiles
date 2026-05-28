42b489ed550901d3f4f4cb056ce69ad027c5ae4f
Andreas Nygard <andreas.nygard@gmail.com>
2026-05-28 10:59:07 +1000

    nvim: Show git modifications

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,6 +68,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git modifications' })
 
 -------------------
 -- Neovide
